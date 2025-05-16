8f83e6786b365c15d9b1a6f60a4ef3bc40b61104
Ian Ellis <ianellis015@gmail.com>
2025-05-16 14:18:27 -0700

    chore: initializes project skeleton

---
+++ nestshop/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className="header">
+      <h1>NestShop</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
