f35aa4ba653f26868a712ebd99971ce50d05e67e
Ian Ellis <ianellis015@gmail.com>
2025-05-31 12:56:14 -0700

    Preparing github pages deployment

--- nestshop/src/App.jsx
+++ nestshop/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { fetchAllProducts, fetchProductById } from './api/products';
 import { ThemeProvider } from 'styled-components';
 import { lightTheme, darkTheme } from './theme';
